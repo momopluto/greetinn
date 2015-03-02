7f70f3f29b6557686b0639a8db77a8cee58f2eb0
momopluto <294666880@qq.com>
2015-03-02 09:26:28 +0800

    FIX Home/User/login's limit
    add limit of member’s on_job status
    0离职
    2休假
    1在职

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -29,20 +29,37 @@ class UserController extends Controller{
     		}
     		
     		$one = is_IDCard_exists(I('post.user'), 'member');
+
+            // p($one);die;
     		
 			// 密码通过验证，则检查是否存在该成员，且职位position为前台
 			if ($one && $one['position'] == self::OPER_CATE_RECEPTIONIST) {
-				
-				$info['oper_ID'] = $one['member_ID'];
-				$info['oper_CATE'] = self::OPER_CATE_RECEPTIONIST;
-				// 通过验证，合法，写session
-				session('H_USER_V_INFO', $info);
-				session('H_LOGIN_FLAG', true);
 
-				$Home = A('home'); 
-				$Home->login_log();// 调用Home/Controller/login_log方法
+                switch ($one['on_job']) {
+                    case 0:
+                        $this->error('该成员已离职！');// , U('Home/User/login')
+                        break;
+                    case 2:
+                        $this->error('该成员休假中！');
+                        break;
+                    case 1:
+                        $info['oper_ID'] = $one['member_ID'];
+                        $info['oper_CATE'] = self::OPER_CATE_RECEPTIONIST;
+                        // 通过验证，合法，写session
+                        session('H_USER_V_INFO', $info);
+                        session('H_LOGIN_FLAG', true);
+
+                        $Home = A('home'); 
+                        $Home->login_log();// 调用Home/Controller/login_log方法
+                        
+                        $this->success('登录成功！', U('Home/Index/index'));
+                        break;
+                    default:
+                        break;
+                }
+
+                return;
 				
-				$this->success('登录成功！', U('Home/Index/index'));
 			}else{
 
 				$this->error('账号或密码不正确！', U('Home/User/login'));
